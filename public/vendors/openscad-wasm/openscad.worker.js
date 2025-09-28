604c78a733ee6cf502f07257d6e736662f38019f
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2025-09-28 12:35:32 -0300

    Fix invalid openscad wasm path

--- public/vendors/openscad-wasm/openscad.worker.js
+++ public/vendors/openscad-wasm/openscad.worker.js
@@ -1,10 +1,10 @@
-import OpenSCAD from "/js/openscad-wasm/openscad.js"
+import OpenSCAD from "/vendors/openscad-wasm/openscad.js"
 let instance
 
 onmessage = async ({data}) => {
   const {scad, out = "model.stl", args = []} = data
 
-  instance ??= await OpenSCAD({noInitialRun: true, locateFile: p => `/js/openscad-wasm/${p}`})
+  instance ??= await OpenSCAD({noInitialRun: true, locateFile: p => `/vendors/openscad-wasm/${p}`})
 
   instance.FS.writeFile("/input.scad", scad)
   instance.callMain(["/input.scad", "--enable=manifold", "-o", out, ...args])
