8a6312b7fe21880379204bb866540791e02f7c4e
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2025-09-28 11:56:27 -0300

    Skip render when codes not change

--- public/js/application.js
+++ public/js/application.js
@@ -25,10 +25,37 @@ document.addEventListener('DOMContentLoaded', async () => {
     checksum: await sha1(defaultCode)
   }
 
-  // load saved code from localStorage if exists
-  const savedContent = localStorage.getItem(localStorageCodeKey);
-  if (savedContent) {
-    editorElement.value = savedContent;
+  // codemirror editor setup
+  CodeMirror.defineSimpleMode(SCAD_NAME, SCAD_MODE)
+
+  const editorStateUpdate = async (currentState = {}) => {
+    currentState.checksum = await sha1(currentState.code)
+
+    const previousState = fetchState(codeKey, {})
+    if (previousState.checksum !== currentState.checksum) setState(codeKey, currentState)
+
+    return currentState
+  }
+
+  const hasEditorChanged = async (currentState = {}) => {
+    currentState.checksum = await sha1(currentState.code)
+
+    const previousState = fetchState(codeKey, {})
+    return previousState.checksum != currentState.checksum
+  }
+
+  const needBuild = async (currentState = {}) => {
+    currentState.checksum = await sha1(currentState.code)
+
+    const previousState = fetchState(codeKey, {})
+
+    const needToBeUpdated = (previousState.lastBuild !== currentState.checksum)
+    console.log(previousState.checksum, currentState.checksum, needToBeUpdated)
+    console.log(previousState, currentState)
+    if (!needToBeUpdated) return false
+
+    setState(codeKey, {...previousState, ...currentState, lastBuild: currentState.checksum})
+    return true
   }
 
   // codemirror editor instance
@@ -37,70 +64,70 @@ document.addEventListener('DOMContentLoaded', async () => {
     mode: 'scad',
     theme: 'material',
     lineWrapping: true,
-  });
+  })
 
-  // apply saved font size
-  editor.getWrapperElement().style.fontSize = `${settings.editorFontSize}px`;
+  const editorState = fetchState(codeKey, defaulEditorState)
+  if (editorState) editor.setValue(editorState.code || defaultCode)
 
-  // save code to localStorage on change
+
+  // save code state every change
   editor.on('change', () => {
-    const content = editor.getValue();
-    localStorage.setItem(localStorageCodeKey, content);
-  });
+    const currentState = fetchState(codeKey, {})
+    currentState.code = editor.getValue()
+    editorStateUpdate(currentState)
+  })
 
   // blob caching
-  const CACHE_NAME = 'openscad_model_cache';
-
-  // save model (stl blob) to cache
-  const cacheStore = async (blob, name) => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.put(name, new Response(blob));
-  };
-
-  // fetch model (stl blob) from cache
-  const cacheFetch = async (name) => {
-    const cache = await caches.open(CACHE_NAME);
-    const response = await cache.match(name);
-    if (response) {
-      return response.blob();
-    }
-    return undefined;
-  };
+  const CACHE_NAME = 'openscad_model_cache'
 
   // editor tab
-  const editorTab = document.getElementById('nav-tab-editor');
-  editorTab.addEventListener('shown.bs.tab', () => {
-    editor.refresh();
-  });
+  const editorTab = document.getElementById('nav-tab-editor')
+  editorTab.addEventListener('shown.bs.tab', async () => {
+    const changed = await hasEditorChanged({code: editor.getValue()})
+    console.log('Editor changed?', changed)
+    editor.refresh()
+  })
 
   // preview tab
   const previewTab = document.getElementById('nav-tab-preview')
   previewTab.addEventListener('shown.bs.tab', async () => {
-    const openscad = await OpenSCAD({noInitialRun: true, locateFile: (path) => `/js/openscad-wasm/${path}`})
+    const needToBuildModel = await needBuild({code: editor.getValue()})
 
+    // skip initial build on page load and
+    //  skips build if code checksum is diferent that last build checksum
+    if (!needToBuildModel && !window.FIRST_RUN) {
+      console.log('Skipping build, code not changed')
+      return
+    }
+
+    console.log([needToBuildModel, window.FIRST_RUN])
+    window.FIRST_RUN = false
+
+    // TODO: show loading status
+
+    // build model
+    const openscad = await OpenSCAD({noInitialRun: true, locateFile: (path) => `/vendors/openscad-wasm/${path}`})
     const scad = editor.getValue()
 
-    openscad.FS.writeFile("/input.scad", scad)
-    openscad.callMain(["/input.scad", "--enable=manifold", "-o", "model.stl"])
+    let blob
+    try {
+      openscad.FS.writeFile("/input.scad", scad)
+      openscad.callMain(["/input.scad", "--enable=manifold", "-o", "model.stl"])
 
-    const bytes = openscad.FS.readFile("/model.stl")
-    const blob = new Blob([bytes], {type: "model/stl"})
-    // await cacheStore(blob, 'model.stl');
+      const bytes = openscad.FS.readFile("/model.stl")
+      blob = new Blob([bytes], {type: "model/stl"})
+    }
+    catch (e) {
+      alert('Compile Error: cannot be able to compile scad.')
+      return
+    }
+
+    // await cacheStore(blob, 'model.stl')
     const url = URL.createObjectURL(blob)
 
-    const viewer = document.getElementById("preview-canvas")
+    const viewer = document.getElementById("preview-model")
     viewer.model = url
 
-    console.log("STL model loaded into viewer")
-    URL.revokeObjectURL(url);
-  });
-
-  // load cached blob if exists
-  if (savedContent) {
-    // const retrivedBlob = await cacheFetch('model.stl')
-    // if (retrivedBlob) {
-    //   const blob = await retrievedBlob.text()
-    //   console.log('Content of retrieved blob:', blob);
-    // }
-  }
-});
+    URL.revokeObjectURL(url)
+  })
+})
